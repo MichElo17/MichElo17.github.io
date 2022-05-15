85342f0f828d87ca32a45bb1e2b184805090de70
Michelo11 <michele.manna@istitutomontani.edu.it>
2022-05-15 19:21:59 +0200

    various fixes

---
+++ script.js
@@ -0,0 +1,45 @@
+let menu = document.querySelector('#menu')
+let button = document.querySelector('#button')
+
+function isOpen() {
+    return menu.classList.contains("nav-open");
+}
+
+function openMenu(){
+    button.classList.toggle('opened');
+    button.setAttribute('aria-expanded', button.classList.contains('opened'));
+
+    menu.classList.remove("nav-close");
+    menu.classList.add("nav-open");
+}
+
+function closeMenu(){
+    button.classList.toggle('opened');
+    button.setAttribute('aria-expanded', button.classList.contains('opened'));
+
+    menu.classList.remove("nav-open");
+    menu.classList.add("nav-close");
+}
+
+$("form").submit(function (event) {
+    event.preventDefault();
+
+    const data = new FormData();
+    data.append("access_key", "92d5b5b6-f330-4590-b112-4ea24d1fc9a2");
+    data.append("name", $("#name").val())
+    data.append("email",$("#email").val())
+    data.append("message",$("#message").val())
+
+    axios({
+        method: "post",
+        url: "https://api.web3forms.com/submit",
+        data: data,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+    alert("Message sent.");
+})
+
+$("#view-more").click(function (event) {
+    document.getElementById('works').scrollIntoView();
+})
